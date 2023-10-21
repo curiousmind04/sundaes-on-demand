b3faf4841540e53ac8ea54a3c9e9e1e0ace4369b
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-10-21 19:22:57 -0400

    code and test topping options images from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
 
 const Options = ({ optionType }) => {
@@ -16,8 +17,7 @@ const Options = ({ optionType }) => {
       });
   }, [optionType]);
 
-  //TODO: replace null with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
