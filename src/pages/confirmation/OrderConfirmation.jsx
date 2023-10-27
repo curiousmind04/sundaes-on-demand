ed8b107210860de3d55135797d139ee8beb1b2ca
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-10-26 20:43:48 -0400

    test and code error response on OrderConfirmation page

--- src/pages/confirmation/OrderConfirmation.jsx
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -2,20 +2,33 @@ import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "../../contexts/OrderDetails";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AlertBanner from "../common/AlertBanner";
 
 const OrderConfirmation = ({ setOrderPhase }) => {
   const { resetOrder } = useOrderDetails();
   const [orderNumber, setOrderNumber] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       //in a real app we would get order details from context and send with POST
       .post(`http://localhost:3030/order`)
       .then((response) => setOrderNumber(response.data.orderNumber))
-      .catch((error) => {
-        //TODO
-      });
+      .catch((error) => setError(true));
   }, []);
+
+  const newOrderButton = (
+    <Button onClick={handleClick}>Create new order</Button>
+  );
+
+  if (error) {
+    return (
+      <>
+        <AlertBanner message={null} variant={null} />
+        {newOrderButton}
+      </>
+    );
+  }
 
   function handleClick() {
     // clear the order details
@@ -33,7 +46,7 @@ const OrderConfirmation = ({ setOrderPhase }) => {
         <p style={{ fontSize: "25%" }}>
           as per our terms and conditions, nothing will happen now
         </p>
-        <Button onClick={handleClick}>Create new order</Button>
+        {newOrderButton}
       </div>
     );
   } else {
---
+++ src/pages/confirmation/test/OrderConfirmation.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "../../../test-utils/testing-library-utils";
+import OrderConfirmation from "../OrderConfirmation";
+import { rest } from "msw";
+import { server } from "../../../mocks/server.js";
+
+test("error response from server for submitting order", async () => {
+  //override default msw response for options endpoint with error response
+  server.resetHandlers(
+    rest.post("http://localhost:3030/order", (req, res, ctx) =>
+      res(ctx.status(500))
+    )
+  );
+
+  render(<OrderConfirmation />);
+
+  const alert = await screen.findByRole("alert");
+  expect(alert).toHaveTextContent(
+    "An unexpected error occurred. Please try again later."
+  );
+});
